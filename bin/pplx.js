97a9fc8ac4f6d52e1c433d49132d411577ca923f
Ubuntu <ubuntu@ip-172-31-39-18.eu-north-1.compute.internal>
2026-02-01 17:11:38 +0000

    fix: add node: protocol to builtin imports

--- bin/pplx.js
+++ bin/pplx.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { spawn, execSync } from 'child_process';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
